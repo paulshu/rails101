365ac7842c980ff44130a20d965704ca0d0df4f1
paulshu <paulshu18@gmail.com>
2017-01-14 21:44:26 +0800

    implement my post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,31 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
 
+  end
+# 附加作业
+
+  def update
+    @group = Group.find(params[:group_id])
+    @group = Post.find(params[:id])
+    @post.group = @group
+    @post.user = current_user
+    if @post.update(post_params)
+      redirect_to posts_account_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @group = Post.find(params[:id])
+
+    @group.destroy
+    redirect_to post_account_path(@group), alert: "Post deleted"
+  end
+#附加作业
   private
 
   def post_params
